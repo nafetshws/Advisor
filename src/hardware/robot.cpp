442f8356a2dabead06a262c6a03f1d5274e2df41
nafetshws <stefan0507hws21@gmail.com>
2024-05-12 19:10:54 +0200

    added abstraction layer for hardware

---
+++ src/hardware/robot.cpp
@@ -0,0 +1,26 @@
+#include "../include/robot.hpp"
+
+
+bool Robot::wallFront() {
+    return false;
+}
+
+bool Robot::wallRight() {
+    return false;
+}
+
+bool Robot::wallLeft() {
+    return false;
+}
+
+void Robot::moveForward(int distance = 1) {
+
+}
+
+void Robot::turnRight() {
+
+}
+
+void Robot::turnLeft() {
+
+}
