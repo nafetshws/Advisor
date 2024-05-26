95b122430a761c679b914172ab639f16e95e792a
ZipFH <105421740+ZipFH@users.noreply.github.com>
2024-05-26 17:18:28 +0200

    Changed ToF to support 2 and 4
    Removed Support for 6 TOF
    Added support for 2 and 4 TOF sensors

--- src/hardware/tof.cpp
+++ src/hardware/tof.cpp
@@ -77,3 +77,50 @@ void initTofSensors(TOF &tof1, TOF &tof2) {
   delay(10);
 }
 
+
+void initTofSensors(TOF &tof1, TOF &tof2, TOF &tof3, TOF &tof4) {
+
+  // reset all sensor
+  tof1.enable(false);
+  tof2.enable(false);
+  tof3.enable(false);
+  tof4.enable(false);
+  delay(10);
+
+  // unreset all sensors
+  tof1.enable(true);
+  tof2.enable(true);
+  tof3.enable(true);
+  tof4.enable(true);
+  delay(10);
+
+  // reset all sensors again
+  tof1.enable(false);
+  tof2.enable(false);
+  tof3.enable(false);
+  tof4.enable(false);
+  delay(10);
+  
+  // only enable one at a time to set the address
+  
+  // tof 1
+  tof1.enable(true);
+  tof1.begin();
+  delay(10);
+
+  // tof 2
+  tof2.enable(true);
+  tof2.begin();
+  delay(10);
+
+  // tof 3
+  tof3.enable(true);
+  tof3.begin();
+  delay(10);
+
+  // tof 4
+  tof4.enable(true);
+  tof4.begin();
+  delay(10);
+}
+
